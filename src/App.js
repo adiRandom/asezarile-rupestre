e6c8e4e3d9b2c4ef7faeebe35a5b9217017b0961
Adrian Pascu <dev.adi.hf@gmail.com>
2018-09-06 13:43:00 +0300

    v0.3.2-pre
    -Started omplementing a new map api

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Route, Switch} from "react-router";
 import Homepage from "./components/homepage";
-import MapContainer from "./components/map";
+import MapContainer from "./components/mapContainer";
 
 
 class App extends Component {
