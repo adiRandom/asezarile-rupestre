c5374e739399df83b75217b740730bbc300f5cc0
Adrian Pascu <dev.adi.hf@gmail.com>
2018-09-11 15:37:35 +0300

    0.3.3-pre2
    -Changed to a new project without npm eject

---
+++ src/components/homepage.js
@@ -0,0 +1,67 @@
+/**
+ * Created by Adrian on 29-Aug-18.
+ */
+
+import React, {Component} from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Link} from "react-router-dom";
+import background from "../assets/img/background.jpg";
+import next from "../assets/icons/next.png";
+import {CSSTransitionGroup} from "react-transition-group";
+import "../assets/stylesheets/animation.css";
+class Homepage extends Component {
+
+
+    constructor() {
+
+        super();
+
+        //Manipulate the page background
+        document.getElementsByTagName("body")[0].style.backgroundImage = `url(${background})`;
+        document.getElementsByTagName("body")[0].style.backgroundSize = `cover`;
+        document.getElementsByTagName("body")[0].style.backgroundRepeat = `no-repeat`;
+    }
+
+
+    render() {
+        return (
+
+
+            <div className="container">
+                <div className="row align-items-end" style={{height: "50vh"}}>
+                    <div className="col-12">
+
+                        <CSSTransitionGroup
+                            transitionName="title"
+                            transitionAppear={true}
+                            transitionAppearTimeout={3000}
+                            transitionEnter={false}
+                            transitionLeave={false}>
+                            <h1 className="text-light text-center display-3">Asezarile Rupestre Bozioru</h1>
+                        </CSSTransitionGroup>
+
+                    </div>
+                </div>
+                <div className="row justify-content-end align-items-center" style={{height: "20vh"}}>
+                    <div className="col-3">
+                        <CSSTransitionGroup
+                            transitionName="next"
+                            transitionAppear={true}
+                            transitionAppearTimeout={3000}
+                            transitionEnter={false}
+                            transitionLeave={false}>
+                            <Link to="/map" className="text-light" style={{textDecoration: 'none'}}><span><img
+                                alt="next"
+                                src={next}/></span>Catre
+                                harta</Link>
+                        </CSSTransitionGroup>
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Homepage;
+
---
+++ src/components/map.js
@@ -0,0 +1,292 @@
+/**
+ * Created by Adrian on 29-Aug-18.
+ */
+
+import React, {Component} from "react";
+import {properties} from "../properties/properties";
+import {GoogleApiWrapper, Map} from "google-maps-react";
+import "bootstrap/dist/js/bootstrap.min";
+import * as classNames from "classnames";
+import next from "../assets/icons/next.png";
+import "../assets/stylesheets/main.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+export class MapContainer extends React.Component {
+
+
+    //TODO:After romania and buzao switch to a img of the map outlined
+    //TODO:Photo over the map after bozioru
+
+    constructor() {
+
+        super();
+
+        //Manipulate the page background
+        document.getElementsByTagName("body")[0].style.background = `linear-gradient(45deg, #00537e 0%,#3aa17e 100%)`;
+        document.getElementsByTagName("body")[0].style.backgroundRepeat = `no-repeat`;
+        document.getElementsByTagName("body")[0].style.backgroundAttachment = `fixed`;
+        //Init the state
+        this.state = {
+            zoom: properties.romania_map_properties.zoom,
+            initCenter: properties.romania_map_properties.center,
+            center: properties.romania_map_properties.center,
+            stage: 0,
+            classes: {
+
+                romania: classNames({
+                    'active': true,
+                    'nav-link': true
+                }),
+                buzau: classNames({
+                    'active': false,
+                    'nav-link': true
+                }),
+                bozioru: classNames({
+                    'active': false,
+                    'nav-link': true
+                }),
+
+            },
+            endReached: false // Property to track whether you can advance to the next slide or not
+        }
+
+        this.clickHandler = this.clickHandler.bind(this);
+        this.goToRomania = this.goToRomania.bind(this);
+        this.goToBuzau = this.goToBuzau.bind(this);
+        this.goToBozioru = this.goToBozioru.bind(this);
+        this.zoomContinuation = this.zoomContinuation.bind(this);
+        this.toDemo = this.toDemo.bind(this);
+
+        this.mapRef = React.createRef();
+
+    }
+
+    componentDidMount() {
+
+        /*var directionsService = new window.google.maps.DirectionsService;
+         var directionsDisplay = new window.google.maps.DirectionsRenderer;
+         directionsDisplay.setMap(this.mapRef.current.map);
+         directionsService.route({
+         origin: "Buzau",
+         destination: "Brasov",
+         travelMode: 'DRIVING'
+         }, function(response, status) {
+         if (status === 'OK') {
+         directionsDisplay.setDirections(response);
+         } else {
+         window.alert('Directions request failed due to ' + status);
+         }
+         });*/
+
+    }
+
+
+    //TODO: Make the demo open in a new tab
+
+    toDemo() {
+
+        let window = window.open('/demo', 'Tour');
+        window.focus();
+
+    }
+
+    clickHandler() {
+
+        //After every click move to the next stage of the presentation
+
+        if (this.state.stage === 0) {
+            this.setState({
+                zoom: properties.buzau_map_properties.zoom,   //Buzau stage
+                center: properties.buzau_map_properties.center,
+                stage: 1,
+                classes: {
+
+                    romania: classNames({
+                        'active': false,
+                        'nav-link': true
+                    }),
+                    buzau: classNames({
+                        'active': true,
+                        'nav-link': true
+                    }),
+                    bozioru: classNames({
+                        'active': false,
+                        'nav-link': true
+                    }),
+
+                }
+            });
+
+            setTimeout(this.clickHandler, 3000); //Move along
+        }
+        else if (this.state.stage === 1) {
+
+            this.setState({
+                zoom: properties.bozioru_map_properties.zoom - 3.5,   //Bozioru intermediate stage - zoom efect not working when zooming to much to fast
+                center: properties.bozioru_map_properties.center,
+                stage: 2,
+                classes: {
+
+                    romania: classNames({
+                        'active': false,
+                        'nav-link': true
+                    }),
+                    buzau: classNames({
+                        'active': false,
+                        'nav-link': true
+                    }),
+                    bozioru: classNames({
+                        'active': true,
+                        'nav-link': true
+                    }),
+
+                },
+                endReached: true
+            }, () => setTimeout(this.zoomContinuation, 558));  //Time required between the stages
+
+        }
+    }
+
+    zoomContinuation() {
+
+        this.setState({zoom: properties.bozioru_map_properties.zoom});  //Finish the zoom to Bozioru
+
+    }
+
+
+    //Function to handle the Romania nav
+
+    goToRomania() {
+
+        this.setState({
+            zoom: properties.romania_map_properties.zoom,   //Romania link
+            center: properties.romania_map_properties.center,
+            stage: 0,
+            classes: {
+
+                romania: classNames({
+                    'active': true,
+                    'nav-link': true
+                }),
+                buzau: classNames({
+                    'active': false,
+                    'nav-link': true
+                }),
+                bozioru: classNames({
+                    'active': false,
+                    'nav-link': true
+                }),
+
+            }
+        });
+
+    }
+
+
+    goToBuzau() {
+
+        this.setState({
+            zoom: properties.buzau_map_properties.zoom,   //Buzau link
+            center: properties.buzau_map_properties.center,
+            stage: 1,
+            classes: {
+
+                romania: classNames({
+                    'active': false,
+                    'nav-link': true
+                }),
+                buzau: classNames({
+                    'active': true,
+                    'nav-link': true
+                }),
+                bozioru: classNames({
+                    'active': false,
+                    'nav-link': true
+                }),
+
+            }
+        });
+
+    }
+
+
+    goToBozioru() {
+
+        //TODO: After a couple of seconds add the path (code sample above)
+
+        this.setState({
+            zoom: properties.bozioru_map_properties.zoom,   //Bozioru link
+            center: properties.bozioru_map_properties.center,
+            stage: 2,
+            classes: {
+
+                romania: classNames({
+                    'active': false,
+                    'nav-link': true
+                }),
+                buzau: classNames({
+                    'active': false,
+                    'nav-link': true
+                }),
+                bozioru: classNames({
+                    'active': true,
+                    'nav-link': true
+                }),
+
+            },
+            endReached: true
+        });
+
+    }
+
+    render() {
+
+        if (this.state.endReached)
+
+            var links = ( <React.Fragment>
+                <a className={this.state.classes.romania} data-toggle="pill" role="tab"
+                   onClick={this.goToRomania}>ROMANIA</a>
+                <a className={this.state.classes.buzau} data-toggle="pill" role="tab" onClick={this.goToBuzau}>BUZAU</a>
+                <a className={this.state.classes.bozioru} data-toggle="pill" role="tab"
+                   onClick={this.goToBozioru}>BOZIORU</a>
+                <a className="nav-link" onClick={this.toDemo}>Catre demo<span><img alt="next" src={next}/></span></a>
+            </React.Fragment>)
+        else
+            var links = ( <React.Fragment>
+                <a className={this.state.classes.romania} data-toggle="pill" role="tab"
+                   onClick={this.goToRomania}>ROMANIA</a>
+                <a className={this.state.classes.buzau} data-toggle="pill" role="tab" onClick={this.goToBuzau}>BUZAU</a>
+                <a className={this.state.classes.bozioru} data-toggle="pill" role="tab"
+                   onClick={this.goToBozioru}>BOZIORU</a>
+            </React.Fragment>)
+
+        return (
+
+            <div className="mapContainer">
+                <div className="container ">
+                    <div className="row w-100 m-0">
+                        <div className="col-2" style={{height: "85vh", padding: 0}}>
+                            <div className="nav flex-column nav-pills nav-fill text-dark" id="v-pills-tab"
+                                 role="tablist"
+                                 aria-orientation="vertical">
+                                {links}
+                            </div>
+                        </div>
+                        <div className="col-10" style={{height: "85vh", padding: 0}}>
+                            <Map google={this.props.google} style={{height: "100%"}}
+                                 initialCenter={this.state.initCenter} center={this.state.center}
+                                 zoom={this.state.zoom} onClick={this.clickHandler} ref={this.mapRef}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default GoogleApiWrapper({
+    apiKey: properties.maps_api_key
+})(MapContainer)
+
+
