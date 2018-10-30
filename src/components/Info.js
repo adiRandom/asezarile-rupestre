5fe9ab57efe273401986df4cf281705071b8644e
Adrian Pascu <me.adi.dev@gmail.com>
2018-10-30 22:03:27 +0200

    v0.8.0 pre3 -Changed compoentDidupdate to shouldComponentUpdate

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,54 +3,56 @@ import * as background from '../assets/img/background_main.png';
 import Navbar from './Navbar'
 import InfoDisplay from './InfoDisplay';
 
-export default class Info extends React.Component{
+export default class Info extends React.Component {
 
-constructor(props){
-    super(props);
-    this.state={
-        element:null
+    constructor(props) {
+        super(props);
+        this.state = {
+            element: null
+        }
     }
-}
 
-componentDidMount(){
-    switch(this.props.match.params.id){
-        case 'legende': import('../data/legends.json').then((data)=>{
-            this.setState({
-                element:(<InfoDisplay data={data.content} />)
-            })
-        });break;
-        case 'istorie': import('../data/history.json').then((data) => {
-            this.setState({
-                element: (<InfoDisplay data={data.content} />)
-            })
-        }); break;
-    }
-}
-
-componentDidUpdate(prevProps){
-    if (this.props.match.params.id !== prevProps.match.params.id)
+    componentDidMount() {
         switch (this.props.match.params.id) {
             case 'legende': import('../data/legends.json').then((data) => {
                 this.setState({
                     element: (<InfoDisplay data={data.content} />)
                 })
-            });
-            break;
+            }); break;
             case 'istorie': import('../data/history.json').then((data) => {
                 this.setState({
                     element: (<InfoDisplay data={data.content} />)
                 })
             }); break;
-        default: break;
         }
-}
+    }
 
-render(){
-    return (
-        <div style={{backgroundImage:`url(${background})`,height:'100vh',overflowX:'hidden',backgroundSize:'cover'}}>
-            <Navbar/>
-            {this.state.element}
-        </div>
+    shouldComponentUpdate(nextProps,nextState) {
+        if (this.props.match.params.id !== nextProps.match.params.id && nextProps) {
+            switch (nextProps.match.params.id) {
+                case 'legende': import('../data/legends.json').then((data) => {
+                    this.setState({
+                        element: (<InfoDisplay data={data.content} />)
+                    })
+                }); return true;
+                case 'istorie': import('../data/history.json').then((data) => {
+                    this.setState({
+                        element: (<InfoDisplay data={data.content} />)
+                    })
+                });return true;
+                default: return true;
+            }
+        }
+
+        return true;
+    }
+
+    render() {
+        return (
+            <div style={{ backgroundImage: `url(${background})`, height: '100vh', overflowX: 'hidden', backgroundSize: 'cover' }}>
+                <Navbar />
+                {this.state.element}
+            </div>
         )
     }
 }
