7e976ba67422bdd800d8fdaa81bfe1b470a2938e
Adrian Pascu <me.adi.dev@gmail.com>
2018-11-13 08:59:03 +0200

    v0.10.0 pre 1 -Started a new design

--- src/components/InfoDisplayNoCarousel.js
+++ src/components/InfoDisplayNoCarousel.js
@@ -7,15 +7,11 @@ export default class InfoDisplayNoCarousel extends React.Component {
     constructor(props) {
         super(props);
         this.data = this.props.data;
-        console.log(props);
+        console.log(this.data);
 
         //Map \n to html break
-        if (!Array.isArray(this.data[0].textChopped)) { //Check if the data hasn't been previously mapped already
+        if (!Array.isArray(this.data[0].textFull)) { //Check if the data hasn't been previously mapped already
             for (let i = 0; i < this.data.length; i++) {
-                if(this.data[i].textChopped)
-                    this.data[i].textChopped = this.data[i].textChopped.split('\r\n').map((item, key) => {
-                        return <span key={key}>{item}<br /></span>
-                    });
                 if (this.data[i].textFull)
                     this.data[i].textFull = this.data[i].textFull.split('\r\n').map((item, key) => {
                         return <span key={key}>{item}<br /></span>
@@ -27,13 +23,14 @@ export default class InfoDisplayNoCarousel extends React.Component {
         this.state = {
             fullText: this.data[0].textFull,
             picture: this.data[0].picture,
+            map:this.data[0].map
         }
     }
 
     render() {
         return (
             <React.Fragment>
-                <FullTextDisplayNoFullscreen text={this.state.fullText} picture={this.state.picture} />
+                <FullTextDisplayNoFullscreen text={this.state.fullText} picture={this.state.picture} map={this.state.map} />
             </React.Fragment>
         )
     }
