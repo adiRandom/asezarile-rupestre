5837898e9d534af4e39d7082b10466936326261c
Adrian Pascu <me.adi.dev@gmail.com>
2019-06-12 16:19:19 +0300

    started working on the quiz almost done with the evaluation

--- src/components/Test.js
+++ src/components/Test.js
@@ -3,6 +3,13 @@ import "../assets/stylesheets/test.css";
 import Navbar from './Navbar';
 import Circle from "./Circle";
 import * as backButton from "../assets/icons/back-button.svg"
+import * as article from "../assets/img/article.png"
+import * as tvShow from "../assets/img/tv-show.jpeg"
+import * as magazine from "../assets/img/revista.PNG"
+import * as flayer1 from "../assets/img/flayer1.png"
+import * as drawing from "../assets/img/drawing.jpg"
+import * as banner from "../assets/img/afis muzeu.jpg"
+import quiz from "../data/quiz.json"
 
 export default class Test extends React.Component {
 
@@ -11,9 +18,18 @@ export default class Test extends React.Component {
         this.width = document.documentElement.clientWidth;
         this.height = document.documentElement.clientHeight;
 
+        let answers = [];
+        for(let question of quiz.questions){
+            answers.push(-1);
+        }
+
         this.state = {
             isFront: true,
-            title: ""
+            title: "",
+            backContent: null,
+            score:0,
+            showScore:false,
+            answers:answers
         }
     }
 
@@ -23,11 +39,86 @@ export default class Test extends React.Component {
             isFront: false,
             title: title
         })
+
+        switch (title) {
+            case "Articole în presa locala": this.setState({
+                backContent: (
+                    <img id="article" src={article} alt="article" />
+                )
+            }); break;
+            case "Emisiune TV locală": this.setState({
+                backContent: (
+                    <img id="tv-show" src={tvShow} alt="tv-show" />
+                )
+            }); break;
+            case "Revisă": this.setState({
+                backContent: (
+                    <img id="magazine" src={magazine} alt="magazine" />
+                )
+            }); break;
+            case "Flyere": this.setState({
+                backContent: (
+                    <img id="flayer1" src={flayer1} alt="flayer1" />
+                )
+            }); break;
+            case "Desene": this.setState({
+                backContent: (
+                    <img id="drawing" src={drawing} alt="drawing" />
+                )
+            }); break;
+            case "Postere": this.setState({
+                backContent: (
+                    <img id="banner" src={banner} alt="banner" />
+                )
+            }); break;
+            case "Quiz": this.setState({
+                backContent: this.mapQuiz()
+            }); break;
+            default: break;
+        }
     }
 
-    back(){
+    update = (e,i)=>{
+        e.preventDefault();
+        const answers = this.state.answers;
+        answers[i] = e.target.value;
+        
         this.setState({
-            isFront:true
+            answers:answers
+        })
+    }
+
+    mapQuiz(){
+        return(
+            <form id="quiz" onSubmit={this.checkQuiz}>
+                {quiz.questions.map((item,index)=>{
+                    return (
+                        <React.Fragment>
+                            <h3 className="quiz-header">{item.question}</h3>
+                            {item.answers.map((answer,_index)=>{
+                                return (
+                                    <span>
+                                        <input onChange={(event) => this.update(event, index)} value={_index.toString()} checked={
+                                        this.state.answers[index] == _index.toString()
+                                        } type="radio" name={`radio-group-${index}`}/> {answer}
+                                    </span>
+                                );
+                            })}
+                        </React.Fragment>
+                    )
+                })}
+                <input id="quiz-submit" type="submit" value="Verifică"/>
+                <h4 id="quiz-score" style={{
+                    visibility:this.state.showScore?"visible":"hidden"
+                }}>{`Felicitari! Ai optinut ${this.state.score}/${quiz.questions.length} răspunsuri corecte!`} </h4>
+            </form>
+        )
+    }
+    chekcQuiz(){}
+
+    back = () => {
+        this.setState({
+            isFront: true
         })
     }
 
@@ -62,7 +153,7 @@ export default class Test extends React.Component {
                                 <line className="line" x1={this.width / 2 - 200} y1={this.height / 2 + 100} x2={this.width / 2 - 80} y2={this.height / 2 + 260}></line>                        </svg>
                             <Circle className="circle" cx={this.width / 2} cy={this.height / 2}
                                 fill="cornflowerblue" r="50">Modalități de evaluare</Circle>
-                            <Circle onClick={() => this.handle("Proiecte și concursuri școlare")} className="circle" cx={this.width / 2 - 200} cy={this.height / 2 - 100}
+                            <Circle className="circle" cx={this.width / 2 - 200} cy={this.height / 2 - 100}
                                 fill="cornflowerblue" r="50">Proiecte și concursuri școlare</Circle>
                             <Circle className="circle" cx={this.width / 2 - 300} cy={this.height / 2 - 170}
                                 fill="cornflowerblue" r="50">Euroscola</Circle>
@@ -70,13 +161,13 @@ export default class Test extends React.Component {
                                 fill="cornflowerblue" r="50">Istorie și societate în dimensiune virtuala</Circle>
                             <Circle className="circle" cx={this.width / 2 - 200} cy={this.height / 2 + 100}
                                 fill="cornflowerblue" r="50">Promovare</Circle>
-                            <Circle className="circle" cx={this.width / 2 - 320} cy={this.height / 2 + 260}
-                                fill="cornflowerblue" r="50">Revisă</Circle>
-                            <Circle className="circle" cx={this.width / 2 - 200} cy={this.height / 2 + 260}
+                            <Circle onClick={() => this.handle("Revistă")} className="circle" cx={this.width / 2 - 320} cy={this.height / 2 + 260}
+                                fill="cornflowerblue" r="50">Revistă</Circle>
+                            <Circle onClick={() => this.handle("Articole în presa locala")} className="circle" cx={this.width / 2 - 200} cy={this.height / 2 + 260}
                                 fill="cornflowerblue" r="50">Articole în presa locala</Circle>
-                            <Circle className="circle" cx={this.width / 2 - 80} cy={this.height / 2 + 260}
+                            <Circle onClick={() => this.handle("Emisiune TV locală")} className="circle" cx={this.width / 2 - 80} cy={this.height / 2 + 260}
                                 fill="cornflowerblue" r="50">Emisiune TV locală</Circle>
-                            <Circle className="circle" cx={this.width / 2 + 200} cy={this.height / 2 - 150}
+                            <Circle onClick={() => this.handle("Quiz")} className="circle" cx={this.width / 2 + 200} cy={this.height / 2 - 150}
                                 fill="cornflowerblue" r="50">Quiz</Circle>
                             <Circle className="circle" cx={this.width / 2 + 200} cy={this.height / 2}
                                 fill="cornflowerblue" r="50">Investigație</Circle>
@@ -86,15 +177,15 @@ export default class Test extends React.Component {
                                 fill="cornflowerblue" r="50">Portofolii</Circle>
                             <Circle className="circle" cx={this.width / 2 + 200} cy={this.height / 2 + 150}
                                 fill="cornflowerblue" r="50">Reprezentări grafice</Circle>
-                            <Circle className="circle" cx={this.width / 2 + 80} cy={this.height / 2 + 260}
+                            <Circle onClick={() => this.handle("Flyere")} className="circle" cx={this.width / 2 + 80} cy={this.height / 2 + 260}
                                 fill="cornflowerblue" r="50">Flyere</Circle>
-                            <Circle className="circle" cx={this.width / 2 + 200} cy={this.height / 2 + 260}
+                            <Circle onClick={() => this.handle("Desene")} className="circle" cx={this.width / 2 + 200} cy={this.height / 2 + 260}
                                 fill="cornflowerblue" r="50">Desene</Circle>
-                            <Circle className="circle" cx={this.width / 2 + 320} cy={this.height / 2 + 260}
+                            <Circle onClick={() => this.handle("Postere")} className="circle" cx={this.width / 2 + 320} cy={this.height / 2 + 260}
                                 fill="cornflowerblue" r="50">Postere</Circle>
                         </div>
                     </div>
-                    
+
                     <div id="test-back" style={{
                         transform: `rotateY(${!this.state.isFront ? "0" : "180deg"})`
                     }}>
